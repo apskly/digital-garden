4172eb26d4e1931612c4968ab482733807ebe734
Allison Skly <askly@nd.edu>
2025-05-05 00:55:08 -0400

    Quartz sync: May 5, 2025, 12:55 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -22,7 +22,7 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
-    Component.TagList(),
+    // Component.TagList(),
   ],
   left: [
     Component.PageTitle(),
